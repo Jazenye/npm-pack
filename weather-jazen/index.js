91d0c6334464aebf7649c3accb681b9a88c2788b
Jazenye <865388722@qq.com>
2017-09-27 09:31:41 +0800

    new version
    error message added

--- weather-jazen/index.js
+++ weather-jazen/index.js
@@ -14,22 +14,28 @@ if(process.argv[2]){
 
 axios.get('http://api.jirengu.com/weather.php', data)
     .then(function(response) {
-        // console.log(response.data);
-        console.log("\nToday is: " + response.data["date"]);
-        // console.log(response.data["results"][0]);
-        var weather = response.data["results"][0]
-        console.log("城市： "+weather["currentCity"]);
-        console.log("pm2.5： "+weather["pm25"]+"\n\n");
+        var errMsg = "天配额超限，限制访问";
+        // response.data["message"] === errMsg
+        if (response.data["message"]) {
+            console.log(response.data["message"]);
+        } else {
+            // console.log(response.data);
+            console.log("\nToday is: " + response.data["date"]);
+            // console.log(response.data["results"][0]);
+            var weather = response.data["results"][0];
+            console.log("城市： " + weather["currentCity"]);
+            console.log("pm2.5： " + weather["pm25"] + "\n\n");
 
-        var more = response.data["results"][0].weather_data;
-        // console.log(more)
+            var more = response.data["results"][0].weather_data;
+            // console.log(more)
 
-        for(var i = 0; i < 4; i++){
-            console.log("date: ", more[i]["date"]);
-            console.log("weather: ", more[i]["weather"]);
-            console.log("wind: ", more[i]["wind"]);
-            console.log("temperature: ", more[i]["temperature"]);
-            console.log("-------------")
+            for (var i = 0; i < 4; i++) {
+                console.log("date: ", more[i]["date"]);
+                console.log("weather: ", more[i]["weather"]);
+                console.log("wind: ", more[i]["wind"]);
+                console.log("temperature: ", more[i]["temperature"]);
+                console.log("-------------")
+            }
         }
     })
     .catch(function(error) {
